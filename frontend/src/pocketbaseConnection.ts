2b6e1cb227a39d8fdf999aa57f85fef47ea7843c
b-lanc <jerry.hatulusan.tobing@gmail.com>
2023-12-07 23:09:45 +0700

    Fixed url

--- frontend/src/pocketbaseConnection.ts
+++ frontend/src/pocketbaseConnection.ts
@@ -1,7 +1,7 @@
 import PocketBase from 'pocketbase'
 
 //TODO: somehow properly implement production env, where the url is the backend url
-const url = `localhost:8090`
+const url = `http://localhost:8090`
 const pb = new PocketBase(url)
 
 export default pb
