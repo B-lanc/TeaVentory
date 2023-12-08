98ddf44dd0b13373a63fdc0455812cc8f4de0e50
b-lanc <jerry.hatulusan.tobing@gmail.com>
2023-12-08 19:30:02 +0700

    Styles

--- frontend/src/components/CreateOverlay.vue
+++ frontend/src/components/CreateOverlay.vue
@@ -14,11 +14,21 @@ const cancel = () => {
 
 <template>
   <div @click="toggle()" class="fixed left-0 top-0 z-10 h-screen w-screen bg-black bg-opacity-40">
-    <div class="fixed right-0 z-20 h-screen bg-white">
+    <div class="fixed right-0 z-20 h-screen w-1/3 bg-slate-200">
       <slot></slot>
-      <div>
-        <button @click="cancel()" class="rounded-md bg-slate-50 text-black">cancel</button>
-        <button @click="createRecord()" class="rounded-md bg-red-950 text-white">create</button>
+      <div class="flex justify-center">
+        <button
+          @click="cancel()"
+          class="m-4 h-9 w-16 rounded-sm bg-gray-300 text-black hover:bg-gray-500"
+        >
+          cancel
+        </button>
+        <button
+          @click="createRecord()"
+          class="m-4 h-9 w-16 rounded-sm bg-red-950 text-white hover:bg-red-600"
+        >
+          create
+        </button>
       </div>
     </div>
   </div>
