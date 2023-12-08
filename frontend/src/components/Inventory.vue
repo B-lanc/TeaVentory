2cdadc871c9de39286a6e2d96c76a25c56eb4e52
b-lanc <jerry.hatulusan.tobing@gmail.com>
2023-12-08 14:35:53 +0700

    More progress

--- frontend/src/components/Inventory.vue
+++ frontend/src/components/Inventory.vue
@@ -1,17 +1,52 @@
 <script setup lang="ts">
 import pb from '@/pocketbaseConnection'
+import { ref, onMounted } from 'vue'
 
-const records = await pb.collection('inventory').getList(1, 50, {})
-
-console.log(records)
+const errorMessage = ref('Loading... Please Wait')
+const records = ref()
+onMounted(async () => {
+  try {
+    records.value = await pb.collection('inventory').getList(1, 50, {})
+    console.log(records.value)
+  } catch (e) {
+    records.value = ''
+    errorMessage.value = 'Could not load the inventory'
+  }
+})
 </script>
 
 <template>
   <div class="flex flex-col">
     <div class="flex h-12 justify-between px-3 py-2 font-serif">
-      <h2>Inventory</h2>
+      <h2 class="text-3xl">Inventory</h2>
       <div>Something else idk</div>
     </div>
-    <div class="h-full w-full bg-blue-50 p-5">{{ records }}</div>
+    <div class="h-full w-full bg-blue-50 px-5 text-center">
+      <div v-if="records">
+        <div class="grid grid-cols-12 border border-black bg-blue-200">
+          <h4 class="col-span-2 py-1 hover:cursor-pointer hover:bg-blue-400">Inventory ID</h4>
+          <h4 class="col-span-2 py-1 hover:cursor-pointer hover:bg-blue-400">Name</h4>
+          <h4 class="col-span-1 py-1 hover:cursor-pointer hover:bg-blue-400">Size</h4>
+          <h4 class="col-span-1 py-1 hover:cursor-pointer hover:bg-blue-400">Unit</h4>
+          <h4 class="col-span-1 py-1 hover:cursor-pointer hover:bg-blue-400">Stock</h4>
+          <h4 class="col-span-5 py-1 hover:cursor-pointer hover:bg-blue-400">Supplier</h4>
+        </div>
+        <div
+          v-for="(item, index) in records.items"
+          class="grid grid-cols-12 border-x border-b border-black py-1 font-mono font-light hover:bg-blue-400"
+          :class="index % 2 ? 'bg-blue-100' : 'bg-blue-50'"
+        >
+          <h4 class="col-span-2">{{ item.inv_id }}</h4>
+          <h4 class="col-span-2">{{ item.name }}</h4>
+          <h4 class="col-span-1">{{ item.size }}</h4>
+          <h4 class="col-span-1">{{ item.unit }}</h4>
+          <h4 class="col-span-1">{{ item.stock }}</h4>
+          <h4 class="col-span-5">{{ item.supplier }}</h4>
+        </div>
+      </div>
+      <div v-else>
+        {{ errorMessage }}
+      </div>
+    </div>
   </div>
 </template>
