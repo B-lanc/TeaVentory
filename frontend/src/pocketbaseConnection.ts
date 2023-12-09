adbde25f6f23a829b3866e51af2956a27d5a266a
b-lanc <jerry.hatulusan.tobing@gmail.com>
2023-12-09 19:58:03 +0700

    prettier

--- frontend/src/pocketbaseConnection.ts
+++ frontend/src/pocketbaseConnection.ts
@@ -1,7 +1,7 @@
 import PocketBase from 'pocketbase'
 
 console.log(import.meta.env.VITE_DOMAIN_NAME)
-const url = `http://${import.meta.env.VITE_DOMAIN_NAME || "localhost"}:8090`
+const url = `http://${import.meta.env.VITE_DOMAIN_NAME || 'localhost'}:8090`
 const pb = new PocketBase(url)
 
 export default pb
