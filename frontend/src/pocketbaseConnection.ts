44a26d7e91e036ace29c37407dcd5cc9e57a1e73
b-lanc <jerry.hatulusan.tobing@gmail.com>
2023-12-26 19:42:43 +0700

    Prettier

--- frontend/src/pocketbaseConnection.ts
+++ frontend/src/pocketbaseConnection.ts
@@ -3,10 +3,10 @@ import type { TypedPocketBase } from '@/types/pocketbase-types'
 
 const domain = import.meta.env.VITE_DOMAIN_NAME || 'localhost'
 let url = ''
-if (domain === "localhost"){
-    url = "http://localhost:8080"
+if (domain === 'localhost') {
+  url = 'http://localhost:8080'
 } else {
-    url = "https://${domain}:8443"
+  url = 'https://${domain}:8443'
 }
 const pb = new PocketBase(url) as TypedPocketBase
 
