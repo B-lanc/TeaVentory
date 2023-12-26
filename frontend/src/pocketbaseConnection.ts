5b9d6ab254fbc532774b9ebab1717c7b4bbd29e3
b-lanc <jerry.hatulusan.tobing@gmail.com>
2023-12-26 10:36:11 +0700

    https when using domain name

--- frontend/src/pocketbaseConnection.ts
+++ frontend/src/pocketbaseConnection.ts
@@ -2,8 +2,12 @@ import PocketBase from 'pocketbase'
 import type { TypedPocketBase } from '@/types/pocketbase-types'
 
 const domain = import.meta.env.VITE_DOMAIN_NAME || 'localhost'
-const port = domain === 'localhost' ? 8080 : 8443
-const url = `http://${domain || 'localhost'}:${port}`
+let url = ''
+if (domain === "localhost"){
+    url = "http://localhost:8080"
+} else {
+    url = "https://${domain}:8443"
+}
 const pb = new PocketBase(url) as TypedPocketBase
 
 export default pb
